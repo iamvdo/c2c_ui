b3ce6cdf08359535e3a1c0e369f9fc232537db4d
flob38 <florence.berbey@gmail.com>
2019-04-16 22:59:41 +0200

    Add specific link for image with squared image

---
+++ src/components/association-editor/AssociationImageLink.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <document-link :document="image" class="has-hover-background columns">
+    <div class="column">
+      <figure class="image is-96x96">
+        <img :src="getUrl(image)" :title="image.locales[0].title">
+      </figure>
+    </div>
+    <div class="column is-hidden-mobile">
+      <document-title :document="image" />
+      {{image.author}}
+    </div>
+  </document-link>
+
+</template>
+
+<script>
+  import imageUrls from '@/js/image-urls';
+
+  export default {
+    props: {
+      image: {
+        type: Object,
+        required: true
+      }
+    },
+
+    methods: {
+      getUrl(image) {
+        return imageUrls.getSquared(image);
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
